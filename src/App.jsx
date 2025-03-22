9655562b84dd0b5419d5a7253fd39e4883342f3f
content-cn <87414959+content-cn@users.noreply.github.com>
2025-03-22 06:47:50 +0000

    "week-1-class-1"

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Home from './pages/Home'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <>
+
+            <Navbar/>
+            <Home/>
+            <Footer/>
+
+    </>
+  )
+}
+
+export default App
